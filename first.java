c42d2ada2d63d201e60c316afe81052fa48357ab
Harshitadewani <harshitadewani5@gmail.com>
2024-07-09 18:43:58 +0530

    msg

---
+++ first.java
@@ -0,0 +1,17 @@
+public class first 
+{
+    
+    public static void main(String[] args) 
+    {
+     int x=78;
+     if (x!=79|| x==78 || x==79 & x==89)  //if body executed only one time if you will find true in the if part then you will not go to else part
+     //but in if case you will check all the if whether you get if true then also check the other if
+     {
+        
+     } 
+     if(x==78) 
+     {
+    System.out.println("hi");
+     } 
+    }
+}
